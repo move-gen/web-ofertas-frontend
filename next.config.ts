486e60e405c55a2b10f9d677bb4a5bfe3f58bd09
bpalmes <rmnnpalmes@gmail.com>
2025-08-13 14:51:36 +0100

    errores de build 1

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'fotos.inventario.pro',
-                port: '',
-                pathname: '**',
-            },
-        ],
+  experimental: {
+    css: {
+      lightningcss: false,
     },
+  },
 };
 
 export default nextConfig;
